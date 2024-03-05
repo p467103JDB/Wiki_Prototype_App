3b838412a9e5c0984f6f9c1f3e3dcf3dee3fba40
JackDoobies <p467103@tafe.wa.edu.au>
2024-03-05 16:26:55 +0800

    Updated previous old build

---
+++ Wiki_Prototype/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Wiki_Prototype
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Definition = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.textBox_Data = new System.Windows.Forms.TextBox();
+            this.textBox_Category = new System.Windows.Forms.TextBox();
+            this.textBox_Struct = new System.Windows.Forms.TextBox();
+            this.Save_Button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Load_Button = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Add_Button = new System.Windows.Forms.Button();
+            this.Delete_Button = new System.Windows.Forms.Button();
+            this.Edit_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_Definition
+            // 
+            this.textBox_Definition.Location = new System.Drawing.Point(586, 390);
+            this.textBox_Definition.Multiline = true;
+            this.textBox_Definition.Name = "textBox_Definition";
+            this.textBox_Definition.Size = new System.Drawing.Size(556, 389);
+            this.textBox_Definition.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 844);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1346, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // textBox_Data
+            // 
+            this.textBox_Data.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Data.Location = new System.Drawing.Point(586, 129);
+            this.textBox_Data.Name = "textBox_Data";
+            this.textBox_Data.Size = new System.Drawing.Size(556, 35);
+            this.textBox_Data.TabIndex = 2;
+            // 
+            // textBox_Category
+            // 
+            this.textBox_Category.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Category.Location = new System.Drawing.Point(586, 217);
+            this.textBox_Category.Name = "textBox_Category";
+            this.textBox_Category.Size = new System.Drawing.Size(556, 35);
+            this.textBox_Category.TabIndex = 3;
+            // 
+            // textBox_Struct
+            // 
+            this.textBox_Struct.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Struct.Location = new System.Drawing.Point(586, 297);
+            this.textBox_Struct.Name = "textBox_Struct";
+            this.textBox_Struct.Size = new System.Drawing.Size(556, 35);
+            this.textBox_Struct.TabIndex = 4;
+            // 
+            // Save_Button
+            // 
+            this.Save_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Save_Button.Location = new System.Drawing.Point(236, 709);
+            this.Save_Button.Name = "Save_Button";
+            this.Save_Button.Size = new System.Drawing.Size(129, 70);
+            this.Save_Button.TabIndex = 6;
+            this.Save_Button.Text = "Save File";
+            this.Save_Button.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(579, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(338, 37);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Data Structure Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(579, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 37);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Category";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(579, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 37);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Structure";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(579, 350);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 37);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Definition";
+            // 
+            // Load_Button
+            // 
+            this.Load_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Load_Button.Location = new System.Drawing.Point(371, 709);
+            this.Load_Button.Name = "Load_Button";
+            this.Load_Button.Size = new System.Drawing.Size(129, 70);
+            this.Load_Button.TabIndex = 11;
+            this.Load_Button.Text = "Load File";
+            this.Load_Button.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(50, 129);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(450, 35);
+            this.textBox5.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(43, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 37);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Search";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(50, 204);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(450, 475);
+            this.listView1.TabIndex = 14;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Add_Button
+            // 
+            this.Add_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Add_Button.Location = new System.Drawing.Point(1171, 129);
+            this.Add_Button.Name = "Add_Button";
+            this.Add_Button.Size = new System.Drawing.Size(128, 57);
+            this.Add_Button.TabIndex = 15;
+            this.Add_Button.Text = "Add";
+            this.Add_Button.UseVisualStyleBackColor = true;
+            // 
+            // Delete_Button
+            // 
+            this.Delete_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Delete_Button.Location = new System.Drawing.Point(1171, 217);
+            this.Delete_Button.Name = "Delete_Button";
+            this.Delete_Button.Size = new System.Drawing.Size(128, 57);
+            this.Delete_Button.TabIndex = 16;
+            this.Delete_Button.Text = "Delete";
+            this.Delete_Button.UseVisualStyleBackColor = true;
+            // 
+            // Edit_button
+            // 
+            this.Edit_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Edit_button.Location = new System.Drawing.Point(1171, 297);
+            this.Edit_button.Name = "Edit_button";
+            this.Edit_button.Size = new System.Drawing.Size(128, 57);
+            this.Edit_button.TabIndex = 17;
+            this.Edit_button.Text = "Edit";
+            this.Edit_button.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1346, 866);
+            this.Controls.Add(this.Edit_button);
+            this.Controls.Add(this.Delete_Button);
+            this.Controls.Add(this.Add_Button);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.Load_Button);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Save_Button);
+            this.Controls.Add(this.textBox_Struct);
+            this.Controls.Add(this.textBox_Category);
+            this.Controls.Add(this.textBox_Data);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.textBox_Definition);
+            this.Name = "Form1";
+            this.Text = "Wiki Prototype Program";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_Definition;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TextBox textBox_Data;
+        private System.Windows.Forms.TextBox textBox_Category;
+        private System.Windows.Forms.TextBox textBox_Struct;
+        private System.Windows.Forms.Button Save_Button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Load_Button;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button Add_Button;
+        private System.Windows.Forms.Button Delete_Button;
+        private System.Windows.Forms.Button Edit_button;
+    }
+}
+
--- Wiki_Prototype/Properties/AssemblyInfo.cs
+++ Wiki_Prototype/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Wiki_Prototype_App")]
+[assembly: AssemblyTitle("Wiki_Prototype")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Wiki_Prototype_App")]
+[assembly: AssemblyProduct("Wiki_Prototype")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("be6ae9f2-c4f5-46f0-a975-8ea8b85faa03")]
+[assembly: Guid("453e5f59-9ffe-4970-bc62-dc0b25e3d279")]
 
 // Version information for an assembly consists of the following four values:
 //
