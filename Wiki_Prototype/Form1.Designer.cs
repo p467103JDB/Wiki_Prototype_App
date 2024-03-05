14829448bbfba36c6c1913f8227a6e1e318e468e
JackDoobies <p467103@tafe.wa.edu.au>
2024-03-05 16:51:06 +0800

    Okay, Array Sorted
    Array Sorted, just need to do the rest. O.o

--- Wiki_Prototype/Form1.Designer.cs
+++ Wiki_Prototype/Form1.Designer.cs
@@ -33,23 +33,24 @@
             this.textBox_Data = new System.Windows.Forms.TextBox();
             this.textBox_Category = new System.Windows.Forms.TextBox();
             this.textBox_Struct = new System.Windows.Forms.TextBox();
-            this.Save_Button = new System.Windows.Forms.Button();
+            this.button_Save = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.Load_Button = new System.Windows.Forms.Button();
+            this.button_Load = new System.Windows.Forms.Button();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.listView1 = new System.Windows.Forms.ListView();
-            this.Add_Button = new System.Windows.Forms.Button();
-            this.Delete_Button = new System.Windows.Forms.Button();
-            this.Edit_button = new System.Windows.Forms.Button();
+            this.button_Add = new System.Windows.Forms.Button();
+            this.button_Delete = new System.Windows.Forms.Button();
+            this.button_Edit = new System.Windows.Forms.Button();
+            this.button_Reset = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox_Definition
             // 
-            this.textBox_Definition.Location = new System.Drawing.Point(586, 390);
+            this.textBox_Definition.Location = new System.Drawing.Point(591, 378);
             this.textBox_Definition.Multiline = true;
             this.textBox_Definition.Name = "textBox_Definition";
             this.textBox_Definition.Size = new System.Drawing.Size(556, 389);
@@ -67,7 +68,7 @@
             // textBox_Data
             // 
             this.textBox_Data.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox_Data.Location = new System.Drawing.Point(586, 129);
+            this.textBox_Data.Location = new System.Drawing.Point(591, 117);
             this.textBox_Data.Name = "textBox_Data";
             this.textBox_Data.Size = new System.Drawing.Size(556, 35);
             this.textBox_Data.TabIndex = 2;
@@ -75,7 +76,7 @@
             // textBox_Category
             // 
             this.textBox_Category.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox_Category.Location = new System.Drawing.Point(586, 217);
+            this.textBox_Category.Location = new System.Drawing.Point(591, 205);
             this.textBox_Category.Name = "textBox_Category";
             this.textBox_Category.Size = new System.Drawing.Size(556, 35);
             this.textBox_Category.TabIndex = 3;
@@ -83,26 +84,27 @@
             // textBox_Struct
             // 
             this.textBox_Struct.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox_Struct.Location = new System.Drawing.Point(586, 297);
+            this.textBox_Struct.Location = new System.Drawing.Point(591, 285);
             this.textBox_Struct.Name = "textBox_Struct";
             this.textBox_Struct.Size = new System.Drawing.Size(556, 35);
             this.textBox_Struct.TabIndex = 4;
             // 
-            // Save_Button
+            // button_Save
             // 
-            this.Save_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Save_Button.Location = new System.Drawing.Point(236, 709);
-            this.Save_Button.Name = "Save_Button";
-            this.Save_Button.Size = new System.Drawing.Size(129, 70);
-            this.Save_Button.TabIndex = 6;
-            this.Save_Button.Text = "Save File";
-            this.Save_Button.UseVisualStyleBackColor = true;
+            this.button_Save.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Save.Location = new System.Drawing.Point(241, 697);
+            this.button_Save.Name = "button_Save";
+            this.button_Save.Size = new System.Drawing.Size(129, 70);
+            this.button_Save.TabIndex = 6;
+            this.button_Save.Text = "Save File";
+            this.button_Save.UseVisualStyleBackColor = true;
+            this.button_Save.Click += new System.EventHandler(this.button_Save_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(579, 89);
+            this.label1.Location = new System.Drawing.Point(584, 77);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(338, 37);
             this.label1.TabIndex = 7;
@@ -112,7 +114,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(579, 177);
+            this.label2.Location = new System.Drawing.Point(584, 165);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(154, 37);
             this.label2.TabIndex = 8;
@@ -122,7 +124,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(579, 257);
+            this.label3.Location = new System.Drawing.Point(584, 245);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(156, 37);
             this.label3.TabIndex = 9;
@@ -132,26 +134,27 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(579, 350);
+            this.label4.Location = new System.Drawing.Point(584, 338);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(160, 37);
             this.label4.TabIndex = 10;
             this.label4.Text = "Definition";
             // 
-            // Load_Button
+            // button_Load
             // 
-            this.Load_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Load_Button.Location = new System.Drawing.Point(371, 709);
-            this.Load_Button.Name = "Load_Button";
-            this.Load_Button.Size = new System.Drawing.Size(129, 70);
-            this.Load_Button.TabIndex = 11;
-            this.Load_Button.Text = "Load File";
-            this.Load_Button.UseVisualStyleBackColor = true;
+            this.button_Load.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Load.Location = new System.Drawing.Point(376, 697);
+            this.button_Load.Name = "button_Load";
+            this.button_Load.Size = new System.Drawing.Size(129, 70);
+            this.button_Load.TabIndex = 11;
+            this.button_Load.Text = "Load File";
+            this.button_Load.UseVisualStyleBackColor = true;
+            this.button_Load.Click += new System.EventHandler(this.button_Load_Click);
             // 
             // textBox5
             // 
             this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox5.Location = new System.Drawing.Point(50, 129);
+            this.textBox5.Location = new System.Drawing.Point(55, 117);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(450, 35);
             this.textBox5.TabIndex = 12;
@@ -160,7 +163,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(43, 89);
+            this.label5.Location = new System.Drawing.Point(48, 77);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(124, 37);
             this.label5.TabIndex = 13;
@@ -169,59 +172,74 @@
             // listView1
             // 
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(50, 204);
+            this.listView1.Location = new System.Drawing.Point(55, 192);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(450, 475);
             this.listView1.TabIndex = 14;
             this.listView1.UseCompatibleStateImageBehavior = false;
             // 
-            // Add_Button
+            // button_Add
             // 
-            this.Add_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Add_Button.Location = new System.Drawing.Point(1171, 129);
-            this.Add_Button.Name = "Add_Button";
-            this.Add_Button.Size = new System.Drawing.Size(128, 57);
-            this.Add_Button.TabIndex = 15;
-            this.Add_Button.Text = "Add";
-            this.Add_Button.UseVisualStyleBackColor = true;
+            this.button_Add.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Add.Location = new System.Drawing.Point(1176, 117);
+            this.button_Add.Name = "button_Add";
+            this.button_Add.Size = new System.Drawing.Size(128, 57);
+            this.button_Add.TabIndex = 15;
+            this.button_Add.Text = "Add";
+            this.button_Add.UseVisualStyleBackColor = true;
+            this.button_Add.Click += new System.EventHandler(this.button_Add_Click);
             // 
-            // Delete_Button
+            // button_Delete
             // 
-            this.Delete_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Delete_Button.Location = new System.Drawing.Point(1171, 217);
-            this.Delete_Button.Name = "Delete_Button";
-            this.Delete_Button.Size = new System.Drawing.Size(128, 57);
-            this.Delete_Button.TabIndex = 16;
-            this.Delete_Button.Text = "Delete";
-            this.Delete_Button.UseVisualStyleBackColor = true;
+            this.button_Delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Delete.Location = new System.Drawing.Point(1176, 205);
+            this.button_Delete.Name = "button_Delete";
+            this.button_Delete.Size = new System.Drawing.Size(128, 57);
+            this.button_Delete.TabIndex = 16;
+            this.button_Delete.Text = "Delete";
+            this.button_Delete.UseVisualStyleBackColor = true;
+            this.button_Delete.Click += new System.EventHandler(this.button_Delete_Click);
             // 
-            // Edit_button
+            // button_Edit
             // 
-            this.Edit_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Edit_button.Location = new System.Drawing.Point(1171, 297);
-            this.Edit_button.Name = "Edit_button";
-            this.Edit_button.Size = new System.Drawing.Size(128, 57);
-            this.Edit_button.TabIndex = 17;
-            this.Edit_button.Text = "Edit";
-            this.Edit_button.UseVisualStyleBackColor = true;
+            this.button_Edit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Edit.Location = new System.Drawing.Point(1176, 285);
+            this.button_Edit.Name = "button_Edit";
+            this.button_Edit.Size = new System.Drawing.Size(128, 57);
+            this.button_Edit.TabIndex = 17;
+            this.button_Edit.Text = "Edit";
+            this.button_Edit.UseVisualStyleBackColor = true;
+            this.button_Edit.Click += new System.EventHandler(this.button_Edit_Click);
+            // 
+            // button_Reset
+            // 
+            this.button_Reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Reset.Location = new System.Drawing.Point(1176, 710);
+            this.button_Reset.Name = "button_Reset";
+            this.button_Reset.Size = new System.Drawing.Size(128, 57);
+            this.button_Reset.TabIndex = 18;
+            this.button_Reset.Text = "Reset";
+            this.button_Reset.UseVisualStyleBackColor = true;
+            this.button_Reset.Click += new System.EventHandler(this.button_Reset_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1346, 866);
-            this.Controls.Add(this.Edit_button);
-            this.Controls.Add(this.Delete_Button);
-            this.Controls.Add(this.Add_Button);
+            this.Controls.Add(this.button_Reset);
+            this.Controls.Add(this.button_Edit);
+            this.Controls.Add(this.button_Delete);
+            this.Controls.Add(this.button_Add);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.Load_Button);
+            this.Controls.Add(this.button_Load);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.Save_Button);
+            this.Controls.Add(this.button_Save);
             this.Controls.Add(this.textBox_Struct);
             this.Controls.Add(this.textBox_Category);
             this.Controls.Add(this.textBox_Data);
@@ -241,18 +259,19 @@
         private System.Windows.Forms.TextBox textBox_Data;
         private System.Windows.Forms.TextBox textBox_Category;
         private System.Windows.Forms.TextBox textBox_Struct;
-        private System.Windows.Forms.Button Save_Button;
+        private System.Windows.Forms.Button button_Save;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button Load_Button;
+        private System.Windows.Forms.Button button_Load;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.Button Add_Button;
-        private System.Windows.Forms.Button Delete_Button;
-        private System.Windows.Forms.Button Edit_button;
+        private System.Windows.Forms.Button button_Add;
+        private System.Windows.Forms.Button button_Delete;
+        private System.Windows.Forms.Button button_Edit;
+        private System.Windows.Forms.Button button_Reset;
     }
 }
 
--- Wiki_Prototype/Form1.cs
+++ Wiki_Prototype/Form1.cs
@@ -12,9 +12,57 @@ namespace Wiki_Prototype
 {
     public partial class Form1 : Form
     {
+        // Initialize globals
+        const int Col = 4; // AFAIK, capital first letters are used for public global variables - Pascal Case
+        const int Row = 12;
+        string[,] GlobalArray = new string[Row, Col];
+
         public Form1()
         {
             InitializeComponent();
+            InitializeGlobalArray();
+        }
+
+        private void InitializeGlobalArray()
+        {
+            for (int i = 0; i < Row; i++)
+            {
+
+                for(int j = 0; j < Col; j++)
+                {
+                    GlobalArray[i, j] = "";
+                }
+            }
+        }
+
+        private void button_Add_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Delete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Edit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Reset_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Save_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Load_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
